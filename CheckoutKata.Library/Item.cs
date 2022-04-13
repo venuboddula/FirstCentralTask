d80ef8b36095d4e709d33ab5de1f25e7b97194dd
venuboddula <boddula.venugopal@gmail.com>
2022-04-13 08:54:02 +0100

    Created the required projects. And created Item Class

---
+++ CheckoutKata.Library/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Library
+{
+    public class Item
+    {
+        public string SKU { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,23 @@
+﻿using CheckoutKata.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Tests
+{
+    public class CheckoutTestShould
+    {
+        public CheckoutTestShould()
+        {
+            IEnumerable<Item> items = new List<Item>()
+            {
+                new Item() { SKU="A99", UnitPrice=0.50m },
+                new Item() { SKU="B15", UnitPrice=0.30m },
+                new Item() { SKU="C40", UnitPrice=0.60m }
+            };
+        }
+
+    }
+}
