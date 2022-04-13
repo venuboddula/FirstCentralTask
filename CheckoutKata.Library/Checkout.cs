563e00c5b43db4a89d57829a3ada6294d7a2b6aa
venuboddula <boddula.venugopal@gmail.com>
2022-04-13 09:06:10 +0100

    Created the Scan method which just returns true by default

---
+++ CheckoutKata.Library/Checkout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Library
+{
+    public class Checkout
+    {
+        private readonly IEnumerable<Item> _items;
+        public Checkout(IEnumerable<Item> items)
+        {
+            _items = items;
+        }
+        public bool Scan(Item item)
+        {
+            return true;
+        }
+    }
+}
--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -4,19 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CheckoutKata.Tests
 {
     public class CheckoutTestShould
     {
+        IEnumerable<Item> items;
         public CheckoutTestShould()
         {
-            IEnumerable<Item> items = new List<Item>()
+            items = new List<Item>()
             {
                 new Item() { SKU="A99", UnitPrice=0.50m },
                 new Item() { SKU="B15", UnitPrice=0.30m },
                 new Item() { SKU="C40", UnitPrice=0.60m }
             };
+        }
+
+        [Fact]
+
+        public void Be_Able_To_Scan_An_Item()
+        {
+            //Assign
+            Item item = new Item() { SKU = "A99", UnitPrice = 0.50m };
+            Checkout checkOut = new Checkout(items);
+
+            //Act
+            bool isScanned = checkOut.Scan(item);
+
+            //Assert
+            Assert.True(isScanned);
         }
 
     }
